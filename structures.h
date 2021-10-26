8357177158e6cbf1553ca96dd3f22f8c4553d2c2
elarb1 <90756930+elarb1@users.noreply.github.com>
2021-10-26 11:49:51 +0200

    fichier de structures du jeu

---
+++ structures.h
@@ -0,0 +1,14 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+struct sprite_s{
+    int x; //pos en x
+    int y; //pos en y
+    int h; //hauteur
+    int w; //largeur
+};
+typedef struct sprite_s sprite_t;
+
+
+
+#endif
